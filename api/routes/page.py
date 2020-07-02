d873bb11c390649fb757577475b7f9928cbefd48
leoda <leodangeli11@gmail.com>
2020-07-02 02:56:51 -0300

    cambios

--- api/routes/page.py
+++ api/routes/page.py
@@ -17,7 +17,7 @@ def home(req):
 def get_started(req):
     return render_template('get_started.html')
 
-def gramma(req):
+def grammar(req):
     return render_template('grammar.html')
 
 def simple_present(req):
