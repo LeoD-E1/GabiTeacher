e1fe4de40c8de3d5ccb4e139209e07d7f6c87b90
leoda <leodangeli11@gmail.com>
2020-07-22 13:25:19 -0300

    cambio de uri de la bd

--- api/database/controller.py
+++ api/database/controller.py
@@ -4,15 +4,17 @@ from flask_pymongo import PyMongo
 from api.commons.env import env
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = env.get('MONGODB_URI') + '?retryWrites=false'
+app.config["MONGO_URI"] = env.get("MONGODB_URI") + "?retryWrites=false"
 mongo = PyMongo(app)
 
 
 def find(model, condition):
     return mongo.db[model].find(condition)
 
+
 def find_one(model, condition):
     return mongo.db[model].find_one(condition)
 
-def insert(model, reg):
-    return mongo.db[model].insert_one(reg)
+
+def insert(model, condition):
+    return mongo.db[model].insert_one(condition)
