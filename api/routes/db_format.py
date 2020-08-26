41bcfaab8a0abc19e96f8a8213f1f78b1378819b
leoda <leodangeli11@gmail.com>
2020-08-26 14:14:19 -0300

    dbformat added

---
+++ api/routes/db_format.py
@@ -0,0 +1,41 @@
+learn = [
+    {
+        "title": "Verb Tenses",
+        "img": "img/ouse01.jpg",
+        "tenses": [
+            {
+                "title": "Presente Simple",
+                "subtitle": "Acciones que realizamos cotidianamente",
+                "img": "img/course01.jpg",
+                "url": "/simple_present"
+            },
+            {
+                "title": "Presente Continuo",
+                "subtitle": "Acciones que ocurren en este momento, lo que esta pasando ahora",
+                "img": "img/course02.jpg",
+                "url": "/continuous_present"
+            },
+            {
+                "title": "Pasado Continuo",
+                "subtitle": "Acciones que transcurrieron en el pasado",
+                "img": "img/course02.jpg",
+                "url": "/past_continuous"
+            }
+        ],
+        "subtitle": "Tiempos Verbales que usamos en nuestras conversaciones"
+    },
+    {
+        "title": "Adverbs",
+        "img": "couse01.jpg",
+        "types": [
+            {
+                "title": "Frequency",
+                "subtitle": "Son adverbios que se utilizan para expresar cuantas veces...",
+                "img": "course01.jpg",
+                "url": "/adverbs_frequency",
+            }
+        ],
+        "subtitle": "Los Adverbios nos sirven para expresar....",
+    },
+]
+
